c5ab96993b2d51402179e47d39772f7ed818b4dd
tikeysus <tigrantumanyann@gmail.com>
2024-01-30 20:54:55 -0500

    Did some more coding bat.

--- java_basics/coding_bat/StringTwo.java
+++ java_basics/coding_bat/StringTwo.java
@@ -19,5 +19,91 @@ class StringTwo
         return newString.toString(); 
     }
 
+    public int countHi(String str) 
+    {
+    int count = 0;
+    int i = 0;
+        for (; i < str.length() - 1; i ++)
+        {
+        if ((str.substring(i, i + 2)).equals("hi"))
+            {
+            count ++;
+            }
+    }
+        return (count);
+    }
 
+    public boolean catDog(String str) 
+    {
+    int dog = 0;
+    int cat = 0;
+    int i = 0;
+    for (; i < str.length() - 2; i ++)
+    {
+    if (str.substring(i, i + 3).equals("cat"))
+        {
+        cat ++;
+        }
+    else if (str.substring(i, i + 3).equals("dog"))
+        {
+        dog ++;
+        }
+    }
+    return cat == dog; 
+    }
+
+    public int countCode(String str) 
+    {
+    int count = 0;
+    int i = 0; 
+    for (; i < str.length() - 3; i ++)
+    {
+    if ((str.substring(i, i + 2) + str.charAt(i + 3)).equals("coe"))
+        {
+        count ++;
+        }
+    }
+    return count; 
+    }
+
+    public boolean endOther(String a, String b) 
+    {
+    int alen = a.length();
+    int blen = b.length();
+    String alo = a.toLowerCase();
+    String blo = b.toLowerCase();
+    if (alen < blen)
+    {
+        String bsub = blo.substring(blen - alen, blen);
+        return (alo.equals(bsub));
+    }
+    else
+    {
+        String asub = alo.substring(alen - blen, alen);
+        return (blo.equals(asub));
+    }
+    }
+    //A bit ugly with the amount of variables, but it helped
+    //simplify the code. 
+
+    public boolean xyzThere(String str) 
+    {
+    int i = 0;
+    for (; i < str.length() - 2; i ++)
+    {
+    if (str.substring(i, i + 3).equals("xyz"))
+        {
+        if (i == 0 && str.substring(0, 3).equals("xyz"))
+        {
+            return true;
+        }
+    else if (str.substring(i, i + 3).equals("xyz") && str.charAt(i - 1) != '.')
+        {
+        return true; 
+        }
+        }
+    }
+    return false; 
+    }
+    //If you thought the last one was ugly, wait till you see this. 
 }
