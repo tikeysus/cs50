067d6b7a97afcd952785a2852ed692c821ef08e5
Tigran Tumanyan <tikeysus@Tigrans-MacBook-Pro.local>
2024-02-16 15:49:34 -0500

    Added an eTransfer method, improvements to be made regarding exceptions.

--- java_basics/basic_oop/BankAccount.java
+++ java_basics/basic_oop/BankAccount.java
@@ -26,20 +26,47 @@ public class BankAccount
         return debt; 
     }
 
-    public void withDraw(double amount) throws InvalidWithdrawalAmountException
+    public void withDraw(double amount) 
     {
-        if (amount <= balance && amount > 0)
+        try 
         {
-            balance -= amount;
-        }
-        else
+            if (amount <= balance && amount > 0) 
+            {
+                balance -= amount;
+            } 
+            else 
+            {
+                throw new InvalidWithdrawalAmountException("The withdrawal amount " + amount + " exceeds your balance.");
+            }
+        } 
+        catch (InvalidWithdrawalAmountException e) 
         {
-            throw new InvalidWithdrawalAmountException("The withdrawl amount " + amount + " exceeds your balance. ");
+            System.out.println("Error: " + e.getMessage());
         }
     }
-
+    
     public void deposit(double amount)
     {
         balance += amount; 
+    }
+
+    public static void eTransfer(BankAccount sender, BankAccount receiver, double amount)
+    {
+        try
+        {
+            if (amount < sender.getBalance())
+            {
+                sender.withDraw(amount);
+                receiver.deposit(amount);
+            }
+            else
+            {
+                throw new InvalidWithdrawalAmountException("The withdrawal amount " + amount + " exceeds your balance.");
+            }
+        }
+        catch (InvalidWithdrawalAmountException e)
+        {
+            System.out.println("Error: The " + sender.getaccountHolder() + " did not have enough money to send " + amount + " to the " + receiver.getaccountHolder());
+        }
     }
 }
