428dc4773080bec5a915b1eec0b710431404d83f
Tigran Tumanyan <tikeysus@Tigrans-MacBook-Pro.local>
2024-02-16 11:03:10 -0500

    Added deposit and withdrawal methods.

--- java_basics/basic_oop/BankAccount.java
+++ java_basics/basic_oop/BankAccount.java
@@ -1,17 +1,45 @@
+
 public class BankAccount
 {
-    private int debt, balance;
+    private double debt, balance;
     private String account_holder; 
 
-    public BankAccount(int debt, int balance, String account_holder)
+    public BankAccount(double debt, double balance, String account_holder)
     {
         this.debt = debt;
         this.balance = balance;
         this.account_holder = account_holder;
     }
 
-    public int getBalance()
+    public double getBalance()
     {
         return balance;
+    }
+
+    public String getaccountHolder()
+    {
+        return account_holder;
+    }
+
+    public double getDebt()
+    {
+        return debt; 
+    }
+
+    public void withDraw(double amount) throws InvalidWithdrawalAmountException
+    {
+        if (amount <= balance && amount > 0)
+        {
+            balance -= amount;
+        }
+        else
+        {
+            throw new InvalidWithdrawalAmountException("The withdrawl amount " + amount + " exceeds your balance. ");
+        }
+    }
+
+    public void deposit(double amount)
+    {
+        balance += amount; 
     }
 }
--- java_basics/basic_oop/BankAccountTester.java
+++ java_basics/basic_oop/BankAccountTester.java
@@ -4,5 +4,16 @@ class BankAccountTester
     {
         BankAccount my_account = new BankAccount(45, 400, "Lukey");
         System.out.println(my_account.getBalance());
+        my_account.deposit(400);
+        System.out.println(my_account.getBalance());
+        try
+        {
+            my_account.withDraw(1000);
+        }
+        catch (InvalidWithdrawalAmountException e)
+        {
+            System.out.println("Error: " + e.getMessage());
+        }
+        System.out.println(my_account.getBalance());
     }
 }
