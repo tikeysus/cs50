bff3eb1071cde1bf21e03a88eda340fc906ea5bf
Tigran Tumanyan <tikeysus@Tigrans-MacBook-Pro.local>
2024-02-20 14:53:37 -0500

    Added a method that creates a random 6 digit id for a given account holder.

--- java_basics/basic_oop/BankAccount.java
+++ java_basics/basic_oop/BankAccount.java
@@ -1,8 +1,14 @@
+import java.util.Random;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BankAccount
 {
     private double debt, balance;
+    private int id;
     private String account_holder; 
+    private Map<String, Integer> accountIds = new HashMap<>();
+
 
     public BankAccount(double debt, double balance, String account_holder)
     {
@@ -10,7 +16,7 @@ public class BankAccount
         this.balance = balance;
         this.account_holder = account_holder;
     }
-
+    
     public double getBalance()
     {
         return balance;
@@ -24,6 +30,31 @@ public class BankAccount
     public double getDebt()
     {
         return debt; 
+    }
+
+    public int getaccountId(String account_holder)
+    {   
+        int id;
+        Random random = new Random();
+        StringBuilder accountid = new StringBuilder();
+        if (accountIds.containsKey(account_holder))
+        {
+            return accountIds.get(account_holder); 
+        }
+        
+        else
+        {
+        do {
+        for (int i = 0; i < 6; i ++)
+        {
+            accountid.append(random.nextInt(10)); //generates six random integers for the account id. 
+        }
+        id = Integer.parseInt(accountid.toString());
+        } while (accountIds.containsValue(id));
+
+        accountIds.put(account_holder, id);
+        return id; 
+        }
     }
 
     public void withDraw(double amount) 
--- java_basics/basic_oop/BankAccountTester.java
+++ java_basics/basic_oop/BankAccountTester.java
@@ -2,10 +2,6 @@ class BankAccountTester
 {
     public static void main(String args[]) 
     {
-        BankAccount tikeysus = new BankAccount(0, 20, "huge");
-        BankAccount lukeysus = new BankAccount(0, 250, "massive");
-        BankAccount.eTransfer(tikeysus, lukeysus, 500);
-        System.out.println(tikeysus.getBalance());
-        System.out.println(lukeysus.getBalance());
+        BankAccount my_account = new BankAccount(0, 0, "Tikeysus");
     }
 }
